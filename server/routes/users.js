7cd07cc83fe1cb14e12b2d33d3eef3c66e36a396
Alexander <alex.esbj@gmail.com>
2020-05-29 14:49:54 +0200

    authorization done

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-/* GET users listing. */
+/* GET All users. */
 router.get('/', function(req, res, next) {
   fs.readFile('users.json', (err, data) =>{
     if (err) throw err;
@@ -10,5 +10,28 @@ router.get('/', function(req, res, next) {
     res.send(users);
   })
 });
+
+/* POST Log In user */
+
+router.post('/authorize', function(req, res) {
+  fs.readFile('users.json', (err, data) =>{
+    if (err) throw err;
+
+    var logInAllowed = false;
+    var users = JSON.parse(data);
+
+    for (let i = 0; i < users.length; i++) {
+      const element = users[i];
+
+      if(element.password == req.body.password && element.userName == req.body.userName)
+      {
+        logInAllowed = true;
+      }
+    }
+    res.send(logInAllowed);
+  })
+})
+
+
 
 module.exports = router;
